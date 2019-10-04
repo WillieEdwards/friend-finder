31d15bab3bcb7d20c31636c4453f0de3276a6450
Willie Edwards <gammaray360@gmail.com>
2019-10-04 16:26:53 -0700

    created friends api

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+    {
+        name: "Dennis",
+        photo: "/assets/images/dennis.jpg",
+        scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+    },
+    {
+        name: "Sweet Dee",
+        photo: "/assets/images/dee.jpg",
+        scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+    },
+    {
+        name: "Mac",
+        photo: "/assets/images/mac.jpg",
+        scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+    },
+    {
+        name: "Charlie",
+        photo: "/assets/images/charlie.jpg",
+        scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    },
+    {
+        name: "Frank",
+        photo: "/assets/images/frank.jpg",
+        scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    }
+];
+
+module.exports = friends;
